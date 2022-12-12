a89acc4ca6a842010df6eb0cc05cef33ac5e00fd
Khaleed <ikhwankhalid@student.telkomuniversity.ac.id>
2022-12-13 01:47:46 +0700

    add list products endpoint

---
+++ products/entity.go
@@ -0,0 +1,25 @@
+package products
+
+import "time"
+
+type Product struct {
+	ID          int
+	UserID      int
+	Name        string
+	Description string
+	Price       int
+	UserCount   int
+	Benefits    string
+	Slug        string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+type ProductImage struct {
+	ID        int
+	ProductID int
+	FileName  string
+	IsPrimary int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
