088b7be293a3fb1fd61066cb924f7fcb3eb150a1
Khaleed <ikhwankhalid@student.telkomuniversity.ac.id>
2022-12-13 10:28:04 +0700

    add repository in products

--- products/entity.go
+++ products/entity.go
@@ -3,16 +3,17 @@ package products
 import "time"
 
 type Product struct {
-	ID          int
-	UserID      int
-	Name        string
-	Description string
-	Price       int
-	UserCount   int
-	Benefits    string
-	Slug        string
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
+	ID            int
+	UserID        int
+	Name          string
+	Description   string
+	Price         int
+	UserCount     int
+	Benefits      string
+	Slug          string
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	ProductImages []ProductImage
 }
 
 type ProductImage struct {
--- products/repository.go
+++ products/repository.go
@@ -1,1 +1,36 @@
 package products
+
+import "gorm.io/gorm"
+
+type Repository interface {
+	FindAll() ([]Product, error)
+	FindByID(userID int) ([]Product, error)
+}
+
+type repository struct {
+	db *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) *repository {
+	return &repository{db}
+}
+
+func (r *repository) FindAll() ([]Product, error) {
+	var products []Product
+
+	err := r.db.Preload("ProductImages", "productimages.is_primary = 1").Find(&products).Error
+	if err != nil {
+		return products, err
+	}
+	return products, nil
+}
+
+func (r *repository) FindByID(userID int) ([]Product, error) {
+	var product []Product
+
+	err := r.db.Where("user_id", userID).Preload("ProductImages", "productimages.is_primary = 1").Find(&product).Error
+	if err != nil {
+		return product, err
+	}
+	return product, nil
+}
