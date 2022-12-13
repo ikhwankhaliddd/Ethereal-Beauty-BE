adad3bcff2dde5d7c07554991336779a3ae03d32
Khaleed <ikhwankhalid@student.telkomuniversity.ac.id>
2022-12-13 10:42:27 +0700

    add service in products domain

--- products/service.go
+++ products/service.go
@@ -1,1 +1,28 @@
 package products
+
+type Service interface {
+	GetProducts(userID int) ([]Product, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewService(repository Repository) *service {
+	return &service{repository}
+}
+
+func (s *service) GetProducts(userID int) ([]Product, error) {
+	if userID != 0 {
+		product, err := s.repository.FindByID(userID)
+		if err != nil {
+			return product, err
+		}
+		return product, nil
+	}
+	products, err := s.repository.FindAll()
+	if err != nil {
+		return products, err
+	}
+	return products, nil
+}
