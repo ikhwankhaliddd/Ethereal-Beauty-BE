f44ae9b0e186efcc74fe7fd58d404c4322c0d321
Khaleed <ikhwankhalid@student.telkomuniversity.ac.id>
2023-01-22 21:26:36 +0700

    add gorm tag

--- users/entity.go
+++ users/entity.go
@@ -3,13 +3,13 @@ package users
 import "time"
 
 type User struct {
-	ID             int
-	Username       string
-	Email          string
-	Name           string
-	PasswordHash   string
+	ID             int    `gorm:"id"`
+	Username       string `gorm:"username"`
+	Email          string `gorm:"email"`
+	Name           string `gorm:"name"`
+	PasswordHash   string `gorm:"password_hash"`
 	Role           string
-	AvatarFileName string
+	AvatarFileName string    `gorm:"avatar_file_name"`
 	CreatedAt      time.Time `gorm:"created_at:CURRENT_TIMESTAMP()"`
 	UpdatedAt      time.Time `gorm:"updated_at:CURRENT_TIMESTAMP()"`
 }
